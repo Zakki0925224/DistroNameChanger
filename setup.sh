a2077c2d35f1e8206431f3bca22b3d8c79065d46
Zakki0925224 <miyazakiyuuta15@gmail.com>
2020-05-11 17:24:31 +0900

    一部修正

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-VIEW="[$0]:"
+view="[$0]:"
 
 if [ ! $UID = 0 ]; then
-    echo "${VIEW} Error. Please run with sudo."
+    echo "${view} Error. Please run with sudo."
     exit 0
 
 fi
@@ -11,13 +11,13 @@ fi
 if [ $1 = "install" ]; then
     cp ./DistroNameChanger /usr/bin/
     chmod 755 /usr/bin/DistroNameChanger
-    echo "${VIEW} The installation is complete."
+    echo "${view} The installation is complete."
 
 elif [ $1 = "uninstall" ]; then
     rm /usr/bin/DistroNameChanger
-    echo "${VIEW} The uninstall is complete."
+    echo "${view} The uninstall is complete."
 
 else
-    echo "${VIEW} Error. The command \"${1}\" cannot be recognized."
+    echo "${view} Error. The command \"${1}\" cannot be recognized."
 
 fi
